7e77b9e94829d568cbfda449fc413afc64ac1bf7
suhaskhot22 <khotsuhas93@gmail.com>
2017-03-09 11:53:06 +0530

    first commit

---
+++ mavendemo/src/main/java/com/demo/A.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+public class A {
+
+	
+	public static void main(String[] args) {
+		System.out.println("maven demo");
+	}
+	
+	public void showMethod(){
+		System.out.println("inside show method..");
+	}
+}
